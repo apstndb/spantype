7a43fc4ae80f2f6274cbe2fd63976b19b0a0fcd8
apstndb <803393+apstndb@users.noreply.github.com>
2024-10-07 01:36:48 +0900

    Refactor format.go

--- format.go
+++ format.go
@@ -1,19 +1,20 @@
 package spantype
 
 import (
-	sppb "cloud.google.com/go/spanner/apiv1/spannerpb"
 	"fmt"
 	"strings"
+
+	sppb "cloud.google.com/go/spanner/apiv1/spannerpb"
 )
 
 type StructMode int
 
 const (
-	// StructModeBase prints `STRUCT` type as `STRUCT`
+	// StructModeBase formats `STRUCT` type as `STRUCT`
 	StructModeBase StructMode = iota
-	// StructModeRecursive prints `STRUCT` type with field types. e.g. `STRUCT<INT64, STRUCT<INT64>>`
+	// StructModeRecursive formats `STRUCT` type with field types. e.g. `STRUCT<INT64, STRUCT<INT64>>`
 	StructModeRecursive
-	// StructModeRecursiveWithName prints `STRUCT` type with field types with field name. e.g. `STRUCT<n INT64, s STRUCT<n INT64>>`
+	// StructModeRecursiveWithName formats `STRUCT` type with field types with field name. e.g. `STRUCT<n INT64, s STRUCT<n INT64>>`
 	StructModeRecursiveWithName
 )
 
@@ -31,9 +32,9 @@ const (
 type ArrayMode int
 
 const (
-	// ArrayModeBase prints `ARRAY` type as `ARRAY.`
+	// ArrayModeBase formats `ARRAY` type as `ARRAY.`
 	ArrayModeBase ArrayMode = iota
-	// ArrayModeRecursive prints `ARRAY` type with element type. e.g. `ARRAY<INT64>`
+	// ArrayModeRecursive formats `ARRAY` type with element type. e.g. `ARRAY<INT64>`
 	ArrayModeRecursive
 )
 
@@ -93,25 +94,9 @@ func FormatType(typ *sppb.Type, opts FormatOption) string {
 		}
 		return fmt.Sprintf("ARRAY<%v>", FormatType(typ.GetArrayElementType(), opts))
 	case sppb.TypeCode_PROTO:
-		switch opts.Proto {
-		case ProtoEnumModeBase:
-			break
-		case ProtoEnumModeLeaf:
-			_, after, _ := lastCut(typ.GetProtoTypeFqn(), ".")
-			return after
-		case ProtoEnumModeFull:
-			return typ.GetProtoTypeFqn()
-		}
+		return FormatProtoEnum(typ, opts.Proto)
 	case sppb.TypeCode_ENUM:
-		switch opts.Enum {
-		case ProtoEnumModeBase:
-			break
-		case ProtoEnumModeLeaf:
-			_, after, _ := lastCut(typ.GetProtoTypeFqn(), ".")
-			return after
-		case ProtoEnumModeFull:
-			return typ.GetProtoTypeFqn()
-		}
+		return FormatProtoEnum(typ, opts.Enum)
 	case sppb.TypeCode_STRUCT:
 		if opts.Struct == StructModeBase {
 			break
@@ -122,6 +107,25 @@ func FormatType(typ *sppb.Type, opts FormatOption) string {
 	return FormatTypeCode(code)
 }
 
+// FormatProtoEnum formats `PROTO` or `ENUM` type using ProtoEnumMode.
+// It panics when the input type is not `PROTO` or `ENUM`.
+func FormatProtoEnum(typ *sppb.Type, mode ProtoEnumMode) string {
+	if typ.GetCode() != sppb.TypeCode_PROTO && typ.GetCode() != sppb.TypeCode_ENUM {
+		panic(fmt.Sprintf("precondition failed: TypeCode must be PROTO or ENUM, but %v", typ))
+	}
+
+	switch mode {
+	case ProtoEnumModeLeaf:
+		_, after, _ := lastCut(typ.GetProtoTypeFqn(), ".")
+		return after
+	case ProtoEnumModeFull:
+		return typ.GetProtoTypeFqn()
+	default:
+		return FormatTypeCode(typ.GetCode())
+	}
+}
+
+// FormatTypeCode formats sppb.TypeCode, but it formats unknown type code as `UNKNOWN(int32(code))`. e.g. `UNKNOWN(-1)`
 func FormatTypeCode(code sppb.TypeCode) string {
 	if name, ok := sppb.TypeCode_name[int32(code)]; ok {
 		return name
